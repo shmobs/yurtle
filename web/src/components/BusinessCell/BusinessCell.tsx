6825d718720f31a875a08ab7c0d400e891116ef4
Ari Mendelow <ari@mendelow.me>
2023-08-21 19:07:47 -0700

    Generate business + location cells and install storybook

---
+++ web/src/components/BusinessCell/BusinessCell.tsx
@@ -0,0 +1,29 @@
+import type {
+  FindBusinessQuery,
+  FindBusinessQueryVariables,
+} from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query FindBusinessQuery($id: String!) {
+    business: business(id: $id) {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<FindBusinessQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  business,
+}: CellSuccessProps<FindBusinessQuery, FindBusinessQueryVariables>) => {
+  return <div>{JSON.stringify(business)}</div>
+}
---
+++ web/src/components/LocationCell/LocationCell.tsx
@@ -0,0 +1,29 @@
+import type {
+  FindLocationQuery,
+  FindLocationQueryVariables,
+} from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query FindLocationQuery($id: String!) {
+    location: location(id: $id) {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<FindLocationQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  location,
+}: CellSuccessProps<FindLocationQuery, FindLocationQueryVariables>) => {
+  return <div>{JSON.stringify(location)}</div>
+}
