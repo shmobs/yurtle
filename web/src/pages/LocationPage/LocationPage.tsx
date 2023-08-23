74556254e0b2e9ecc19c9fe505af85f3ad5f40c7
Ari Mendelow <ari@mendelow.me>
2023-08-22 19:47:26 -0700

    Start to handle location page

--- web/src/pages/LocationPage/LocationPage.tsx
+++ web/src/pages/LocationPage/LocationPage.tsx
@@ -1,32 +1,18 @@
-import { Link, navigate, routes } from '@redwoodjs/router'
+import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 interface ILocationPageProps {
-  id?: string
-  gmapsPlaceId?: string
+  id: string
 }
 
-const LocationPage = ({ id, gmapsPlaceId }: ILocationPageProps) => {
-  // if ID is included, then we have already created a record for it
-  // if gmapsPlaceId is included, then we need to create a record for it
-  // if neither are included, then we need to go back to the welcome page
-  if (!id && !gmapsPlaceId) {
-    navigate(routes.welcome())
-  }
-
-  console.log({ id, gmapsPlaceId })
-
+const LocationPage = ({ id }: ILocationPageProps) => {
   return (
     <>
       <MetaTags title="Location" description="Location page" />
 
-      <h1>LocationPage</h1>
+      <h1>LocationPage for location {id}</h1>
       <p>
         Find me in <code>./web/src/pages/LocationPage/LocationPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>location</code>, link to me with `
-        <Link to={routes.location()}>Location</Link>`
       </p>
     </>
   )
