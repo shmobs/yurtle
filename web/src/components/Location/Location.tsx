c33589cbd2c6cee5977a0141a5eda44bea4d4c3e
Perry Raskin <perryraskin@gmail.com>
2023-08-23 23:58:43 -0400

    Create Location component and cell, display json

---
+++ web/src/components/Location/Location.tsx
@@ -0,0 +1,10 @@
+const Location = () => {
+  return (
+    <div>
+      <h2>{'Location'}</h2>
+      <p>{'Find me in ./web/src/components/Location/Location.tsx'}</p>
+    </div>
+  )
+}
+
+export default Location
--- web/src/pages/LocationPage/LocationPage.tsx
+++ web/src/pages/LocationPage/LocationPage.tsx
@@ -1,18 +1,51 @@
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useQuery } from '@redwoodjs/web'
+import Location from 'src/components/Location/Location'
 
 interface ILocationPageProps {
   id: string
 }
 
+const LOCATION_QUERY = gql`
+  query LocationQuery($id: String!) {
+    location(id: $id) {
+      id
+      address
+      gmapsPlaceId
+      businessId
+      business {
+        id
+        name
+        description
+      }
+      events {
+        id
+        name
+      }
+      latitude
+      longitude
+      createdAt
+      updatedAt
+    }
+  }
+`
+
 const LocationPage = ({ id }: ILocationPageProps) => {
+  const { data, loading, error } = useQuery(LOCATION_QUERY, { variables: { id } })
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
   return (
     <>
       <MetaTags title="Location" description="Location page" />
 
       <h1>LocationPage for location {id}</h1>
-      <p>
-        Find me in <code>./web/src/pages/LocationPage/LocationPage.tsx</code>
-      </p>
+      <p>{JSON.stringify(data)}</p>
+      <Location/>
     </>
   )
 }
