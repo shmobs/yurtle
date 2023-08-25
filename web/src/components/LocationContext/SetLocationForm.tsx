f6603cc661ad63da38bb1fe9d84aa75e04961298
Ari Mendelow <ari@mendelow.me>
2023-08-25 13:05:24 -0700

    Start switching form over to popover

--- web/src/components/LocationContext/SetLocationForm.tsx
+++ web/src/components/LocationContext/SetLocationForm.tsx
@@ -1,46 +1,109 @@
+import { Check, ChevronsUpDown } from 'lucide-react'
 import { SearchForAreaQuery } from 'types/graphql'
 
 import { Form, useForm } from '@redwoodjs/forms'
 
-import { Input } from '../ui/input'
-import { Label } from '../ui/label'
+import { Button } from 'src/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from 'src/components/ui/command'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from 'src/components/ui/popover'
+import { cn } from 'src/lib/utils'
 
 import { useSearchForAreaQuery } from './useSearchForAreaQuery'
+
+interface ILocationSuggestion {
+  value: string
+  label: string
+}
 
 interface IFormSetLocation {
   searchText: string
 }
 
-const SetLocationForm = () => {
+interface ISetLocationFormProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+}
+
+const SetLocationForm = ({ open, setOpen }: ISetLocationFormProps) => {
+  const [locationSuggestions, setLocationSuggestions] = React.useState<
+    ILocationSuggestion[]
+  >([])
+  const [value, setValue] = React.useState('')
+
   const formMethods = useForm<IFormSetLocation>({})
 
   const onGetSearchForArea = (data: SearchForAreaQuery) => {
-    console.log('set location autofill suggestions', data)
+    setLocationSuggestions(
+      data.searchForArea?.suggestions.map((suggestion) => ({
+        value: suggestion.mapbox_id,
+        label: `${suggestion.name}, ${suggestion.place_formatted}`,
+      }))
+    )
   }
 
   const { getSearchForArea } = useSearchForAreaQuery(onGetSearchForArea)
 
   return (
-    <Form<IFormSetLocation>
-      formMethods={formMethods}
-      onSubmit={(data) => {
-        console.log('onSubmit data', data)
-      }}
-    >
-      <div className="grid grid-cols-4 items-center gap-4">
-        <Label htmlFor="searchText" className="text-right">
-          Location
-        </Label>
-        <Input
-          id="searchText"
-          placeholder="Seattle, WA"
-          className="col-span-3"
-          onChange={(e) => {
-            getSearchForArea({ variables: { searchText: e.target.value } })
-          }}
-        />
-      </div>
-    </Form>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? locationSuggestions.find((framework) => framework.value === value)
+                ?.label
+            : 'Set location...'}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput
+            placeholder="Seattle, WA"
+            onValueChange={(value) => {
+              getSearchForArea({
+                variables: {
+                  searchText: value,
+                },
+              })
+            }}
+          />
+          <CommandEmpty>No locations found.</CommandEmpty>
+          <CommandGroup>
+            {locationSuggestions.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? '' : currentValue)
+                  setOpen(false)
+                }}
+              >
+                <Check
+                  className={cn(
+                    'mr-2 h-4 w-4',
+                    value === framework.value ? 'opacity-100' : 'opacity-0'
+                  )}
+                />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
   )
 }
 
