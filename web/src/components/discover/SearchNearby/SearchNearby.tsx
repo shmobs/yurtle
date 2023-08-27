ab5c86bfe50b4b680b043873557913605c434b41
Ari Mendelow <ari@mendelow.me>
2023-08-26 21:51:12 -0700

    Nearby locations now does some pretty loading stuff

--- web/src/components/discover/SearchNearby/SearchNearby.tsx
+++ web/src/components/discover/SearchNearby/SearchNearby.tsx
@@ -1,4 +1,5 @@
 import { useSearchLocationContext } from 'src/components/LocationContext/locationContext'
+import Locations from 'src/components/Locations/Locations'
 import NearbyLocationsCell from 'src/components/NearbyLocationsCell'
 
 const SearchNearby = () => {
@@ -7,11 +8,13 @@ const SearchNearby = () => {
   // get user's current location as lat/long
   return (
     <>
-      {searchLocation && (
+      {searchLocation ? (
         <NearbyLocationsCell
           // TODO fix the location type so that it instead accepts lat and lng
           location={`${searchLocation.lat},${searchLocation.lng}`}
         />
+      ) : (
+        <Locations />
       )}
     </>
   )
