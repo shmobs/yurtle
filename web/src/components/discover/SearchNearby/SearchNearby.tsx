88ccd3de130ba194c5993cd7f85ead6e2e816a3f
Ari Mendelow <ari@mendelow.me>
2023-08-26 18:45:55 -0700

    Fix 'search nearby' page

--- web/src/components/discover/SearchNearby/SearchNearby.tsx
+++ web/src/components/discover/SearchNearby/SearchNearby.tsx
@@ -1,18 +1,18 @@
+import { useSearchLocationContext } from 'src/components/LocationContext/locationContext'
 import NearbyLocationsCell from 'src/components/NearbyLocationsCell'
 
-import LocationPrompt from '../../LocationContext/LocationPrompt'
-
 const SearchNearby = () => {
-  const [location, setLocation] = React.useState<string>(null)
+  const { searchLocation } = useSearchLocationContext()
 
-  const handleLocation = (loc: { latitude: number; longitude: number }) => {
-    setLocation(`${loc.latitude},${loc.longitude}`)
-  }
   // get user's current location as lat/long
   return (
     <>
-      <LocationPrompt onLocation={handleLocation} />
-      {location && <NearbyLocationsCell location={location} />}
+      {searchLocation && (
+        <NearbyLocationsCell
+          // TODO fix the location type so that it instead accepts lat and lng
+          location={`${searchLocation.lat},${searchLocation.lng}`}
+        />
+      )}
     </>
   )
 }
