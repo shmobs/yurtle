3363e85591d4ba113fb7042f110b39b5db5faff5
Ari Mendelow <ari@mendelow.me>
2023-08-29 21:07:56 -0700

    Remove line

--- web/src/components/Mapbox/Map.tsx
+++ web/src/components/Mapbox/Map.tsx
@@ -12,7 +12,6 @@ const MapMarker = () => (
     viewBox="0 0 22 27"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
   >
     <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g
