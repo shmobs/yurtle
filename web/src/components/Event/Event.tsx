03ff49acf3646716fbb7e2e7012351be5ee35e56
Ari Mendelow <ari@mendelow.me>
2023-09-02 16:26:43 -0700

    Format event page on larger screens and text format button code

--- web/src/components/Event/Event.tsx
+++ web/src/components/Event/Event.tsx
@@ -20,43 +20,53 @@ const Event = ({ event }: IEventProps) => {
   return (
     <>
       <SimplePageHeader title={name} subtitle={type} />
-      <div>
-        <div className="flex justify-between">
-          <Badge variant="yellow">suggestion</Badge>
-          <Button variant="outline">
-            Create Request
-            <ArrowRight />
-          </Button>
+      <div className="md:flex md:gap-5">
+        {/* Event info */}
+        <div className="md:w-1/2">
+          <div className="flex justify-between">
+            <Badge variant="yellow">suggestion</Badge>
+            <Button variant="outline">
+              Create Request
+              <ArrowRight />
+            </Button>
+          </div>
+          <SectionHeader
+            title={
+              <span>
+                Hosted by{' '}
+                <Link
+                  to={routes.business({ id: location.business.id })}
+                  className="link"
+                >
+                  {location.business.name}
+                </Link>
+              </span>
+            }
+          />
+          <p className="text-base text-gray-500">{description}</p>
         </div>
-        <SectionHeader
-          title={
-            <span>
-              Hosted by{' '}
-              <Link
-                to={routes.business({ id: location.business.id })}
-                className="link"
-              >
-                {location.business.name}
+        {/* Location info */}
+        <div className="md:w-1/2">
+          <div className="mt-5 h-56 w-full overflow-clip rounded-md md:mt-0 md:h-96">
+            <MapView
+              lat={location.latitude}
+              long={location.longitude}
+              zoom={17}
+            />
+          </div>
+          <AddressLink
+            className="link mt-1 block text-center text-sm"
+            text={location.address}
+            searchStr={`${location.business.name}, ${location.address}`}
+          />
+          <div className="mt-4 flex justify-center">
+            <Button asChild>
+              <Link to={routes.location({ id: location.id })}>
+                View other events at this venue
               </Link>
-            </span>
-          }
-        />
-        <p className="text-base text-gray-500">{description}</p>
-      </div>
-      <div className="mt-5 h-56 w-full overflow-clip rounded-md">
-        <MapView lat={location.latitude} long={location.longitude} zoom={17} />
-      </div>
-      <AddressLink
-        className="link mt-1 block text-center text-sm"
-        text={location.address}
-        searchStr={`${location.business.name}, ${location.address}`}
-      />
-      <div className="mt-4 flex justify-center">
-        <Button asChild>
-          <Link to={routes.location({ id: location.id })}>
-            View other events at this venue
-          </Link>
-        </Button>
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   )
