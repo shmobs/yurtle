8c55e92b8537a1162e207bc866e2f0fe055e6d41
Ari Mendelow <ari@mendelow.me>
2023-09-02 17:00:52 -0700

    Formatting

--- web/src/components/Event/Event.tsx
+++ web/src/components/Event/Event.tsx
@@ -3,11 +3,12 @@ import { EventQuery } from 'types/graphql'
 
 import { Link, routes } from '@redwoodjs/router'
 
-import { SimplePageHeader } from 'src/layouts/SiteLayout/SiteLayout'
+import { SimplePageHeader } from 'src/layouts/SiteLayout'
+import { cn } from 'src/lib/utils'
 
-import AddressLink from '../AddressLink/AddressLink'
+import AddressLink from '../AddressLink'
 import MapView from '../Mapbox/Map'
-import SectionHeader from '../SectionHeader/SectionHeader'
+import SectionHeader from '../SectionHeader'
 import { Badge } from '../ui/badge'
 import { Button } from '../ui/button'
 
@@ -17,33 +18,51 @@ interface IEventProps {
 
 const Event = ({ event }: IEventProps) => {
   const { name, type, description, location } = event
+
+  const LocationBtn = ({ className }: { className?: string }) => (
+    <div
+      className={cn(
+        'mt-4 flex justify-center md:mt-5 md:justify-end',
+        className
+      )}
+    >
+      <Button asChild>
+        <Link to={routes.location({ id: location.id })}>
+          View other events at this venue
+        </Link>
+      </Button>
+    </div>
+  )
   return (
     <>
       <SimplePageHeader title={name} subtitle={type} />
-      <div className="md:flex md:gap-5">
+      <div className="md:flex md:min-h-full md:gap-5">
         {/* Event info */}
         <div className="md:w-1/2">
-          <div className="flex justify-between">
-            <Badge variant="yellow">suggestion</Badge>
-            <Button variant="outline">
-              Create Request
-              <ArrowRight />
-            </Button>
+          <div className="md:flex md:h-full md:flex-col">
+            <div className="flex justify-between">
+              <Badge variant="yellow">suggestion</Badge>
+              <Button variant="outline">
+                Create Request
+                <ArrowRight />
+              </Button>
+            </div>
+            <SectionHeader
+              title={
+                <span>
+                  Hosted by{' '}
+                  <Link
+                    to={routes.business({ id: location.business.id })}
+                    className="link"
+                  >
+                    {location.business.name}
+                  </Link>
+                </span>
+              }
+            />
+            <p className="text-base text-gray-500">{description}</p>
+            <LocationBtn className="hidden md:flex" />
           </div>
-          <SectionHeader
-            title={
-              <span>
-                Hosted by{' '}
-                <Link
-                  to={routes.business({ id: location.business.id })}
-                  className="link"
-                >
-                  {location.business.name}
-                </Link>
-              </span>
-            }
-          />
-          <p className="text-base text-gray-500">{description}</p>
         </div>
         {/* Location info */}
         <div className="md:w-1/2">
@@ -59,13 +78,7 @@ const Event = ({ event }: IEventProps) => {
             text={location.address}
             searchStr={`${location.business.name}, ${location.address}`}
           />
-          <div className="mt-4 flex justify-center">
-            <Button asChild>
-              <Link to={routes.location({ id: location.id })}>
-                View other events at this venue
-              </Link>
-            </Button>
-          </div>
+          <LocationBtn className="flex md:hidden" />
         </div>
       </div>
     </>
