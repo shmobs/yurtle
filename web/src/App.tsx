5a192c6c2d84acb415b3c264c4d9028849aa6468
Ari Mendelow <ari@mendelow.me>
2023-09-02 21:27:50 -0700

    Setup oauth on web site

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,9 +7,10 @@ import { Toaster } from '@redwoodjs/web/toast'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import { AuthProvider, useAuth } from './auth'
+import { AuthProvider, useAuth, OAuthProvider } from './auth'
 import { LocationProvider } from './components/LocationContext/locationContext'
 
+import '@spoonjoy/redwoodjs-dbauth-oauth-web/dist/style.css'
 import './index.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -17,18 +18,20 @@ const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
-        <RedwoodApolloProvider useAuth={useAuth}>
-          <LocationProvider>
-            {/*
-             * This component needs to have its type updated, but this is indeed the correct usage
-             * eslint-disable-next-line @typescript-eslint/ban-ts-comment
-             * @ts-ignore */}
-            <SlotProvider>
-              <Toaster />
-              <Routes />
-            </SlotProvider>
-          </LocationProvider>
-        </RedwoodApolloProvider>
+        <OAuthProvider>
+          <RedwoodApolloProvider useAuth={useAuth}>
+            <LocationProvider>
+              {/*
+               * This component needs to have its type updated, but this is indeed the correct usage
+               * eslint-disable-next-line @typescript-eslint/ban-ts-comment
+               * @ts-ignore */}
+              <SlotProvider>
+                <Toaster />
+                <Routes />
+              </SlotProvider>
+            </LocationProvider>
+          </RedwoodApolloProvider>
+        </OAuthProvider>
       </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,5 +1,23 @@
+import {
+  createOAuthClient,
+  createOAuth,
+} from '@spoonjoy/redwoodjs-dbauth-oauth-web'
+
 import { createDbAuthClient, createAuth } from '@redwoodjs/auth-dbauth-web'
+import { toast } from '@redwoodjs/web/toast'
 
 const dbAuthClient = createDbAuthClient()
 
+const onOAuthError = (error: string) => {
+  toast.error(error)
+}
+
+const oAuthClient = createOAuthClient({
+  enabledProviders: { apple: false, github: false, google: false },
+})
+
 export const { AuthProvider, useAuth } = createAuth(dbAuthClient)
+export const { OAuthProvider, useOAuth } = createOAuth(
+  oAuthClient,
+  onOAuthError
+)
