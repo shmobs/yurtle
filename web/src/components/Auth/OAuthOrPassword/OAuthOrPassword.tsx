a48107bc584ccd6427d090c8be86d164ab834724
Ari Mendelow <ari@mendelow.me>
2023-09-03 16:25:20 -0700

    Update version of oauth plugin

--- web/src/components/Auth/OAuthOrPassword/OAuthOrPassword.tsx
+++ web/src/components/Auth/OAuthOrPassword/OAuthOrPassword.tsx
@@ -1,4 +1,31 @@
-const OAuthOrPassword = () => {
+import { OAuthButtons } from '@spoonjoy/redwoodjs-dbauth-oauth-web'
+
+import { useOAuth } from 'src/auth'
+
+import PasswordLoginForm from '../PasswordLoginForm/PasswordLoginForm'
+import PasswordSignupForm from '../PasswordSignupForm/PasswordSignupForm'
+
+interface IOAuthOrPasswordProps {
+  type: 'oauth' | 'password' | undefined
+  action: 'login' | 'signup'
+  onSignupComplete?: () => void
+  onCompleteRedirectUrlOverride?: string
+}
+
+const OAuthOrPassword = ({
+  type,
+  action,
+  onSignupComplete,
+  onCompleteRedirectUrlOverride,
+}: IOAuthOrPasswordProps) => {
+  const { getOAuthUrls } = useOAuth()
+
+  const getOAuthUrlsWithCustomRedirect = () => {
+    if (onCompleteRedirectUrlOverride)
+      return getOAuthUrls({
+        redirectUrlOverride: onCompleteRedirectUrlOverride,
+      })
+  }
   return (
     <div>
       <h2>{'OAuthOrPassword'}</h2>
