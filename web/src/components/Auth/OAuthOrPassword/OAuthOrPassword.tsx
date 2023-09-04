84ef2bf78cc829ef76e3991d7665cf2d12699132
Ari Mendelow <ari@mendelow.me>
2023-09-03 17:17:23 -0700

    Rendering logic

--- web/src/components/Auth/OAuthOrPassword/OAuthOrPassword.tsx
+++ web/src/components/Auth/OAuthOrPassword/OAuthOrPassword.tsx
@@ -6,33 +6,49 @@ import PasswordLoginForm from '../PasswordLoginForm/PasswordLoginForm'
 import PasswordSignupForm from '../PasswordSignupForm/PasswordSignupForm'
 
 interface IOAuthOrPasswordProps {
-  type: 'oauth' | 'password' | undefined
+  type?: 'oauth' | 'password'
   action: 'login' | 'signup'
   onSignupComplete?: () => void
   onCompleteRedirectUrlOverride?: string
 }
 
 const OAuthOrPassword = ({
-  type,
+  type = 'oauth',
   action,
   onSignupComplete,
   onCompleteRedirectUrlOverride,
 }: IOAuthOrPasswordProps) => {
   const { getOAuthUrls } = useOAuth()
 
-  const getOAuthUrlsWithCustomRedirect = () => {
-    if (onCompleteRedirectUrlOverride)
-      return getOAuthUrls({
-        redirectUrlOverride: onCompleteRedirectUrlOverride,
-      })
-  }
+  const [authnType, setAuthnType] = React.useState<'oauth' | 'password'>(type)
+
   return (
-    <div>
-      <h2>{'OAuthOrPassword'}</h2>
-      <p>
-        {'Find me in ./web/src/components/OAuthOrPassword/OAuthOrPassword.tsx'}
+    <>
+      {authnType === 'oauth' ? (
+        <OAuthButtons
+          action={action}
+          getOAuthUrls={getOAuthUrls}
+          redirectUrlOverride={onCompleteRedirectUrlOverride}
+        />
+      ) : action === 'login' ? (
+        <PasswordLoginForm />
+      ) : (
+        <PasswordSignupForm onComplete={onSignupComplete} />
+      )}
+      <p className="mt-10 text-center text-sm text-gray-600">
+        or{' '}
+        <button
+          onClick={() =>
+            setAuthnType(authnType === 'oauth' ? 'password' : 'oauth')
+          }
+          className="link text-center font-medium"
+        >
+          {type === 'oauth' || type === undefined
+            ? 'use your username and password'
+            : 'sign in with a provider'}
+        </button>
       </p>
-    </div>
+    </>
   )
 }
 
