ddcef4fd3d00a153f114b107e6728ba460995a58
Ari Mendelow <ari@mendelow.me>
2023-09-07 15:35:03 -0700

    O turns out you *can* use pk

--- api/src/services/mapSearch/mapSearchUtils.ts
+++ api/src/services/mapSearch/mapSearchUtils.ts
@@ -55,7 +55,7 @@ export async function addMapboxStaticImages<T extends GmapsApiResponseType>(
   // the format is: https://api.mapbox.com/styles/v1/mapbox/dark-v11/static/${lng},${lat},12.5,0/300x200@2x?access_token=${MAPBOX_API_KEY}
   // https://docs.mapbox.com/playground/static/
   responseCopy.results = responseCopy.results.map((result) => {
-    const mapboxStaticImageUrl = `https://api.mapbox.com/styles/v1/rendyapp/cllu728xe005601r980xs80oy/static/${result.geometry.location.lng},${result.geometry.location.lat},16,0/300x200@2x?attribution=false&logo=false&access_token=${process.env.MAPBOX_API_KEY}`
+    const mapboxStaticImageUrl = `https://api.mapbox.com/styles/v1/rendyapp/cllu728xe005601r980xs80oy/static/${result.geometry.location.lng},${result.geometry.location.lat},16,0/300x200@2x?attribution=false&logo=false&access_token=${process.env.MAPBOX_PUBLIC_KEY}`
     result.mapboxStaticImageUrl = mapboxStaticImageUrl
     return result
   }) as T['results']
