3d110ecf3eb1ae93234235ede64e4df0c95d0f60
Ari Mendelow <ari@mendelow.me>
2023-09-09 23:17:48 -0700

    Move about the background so it looks better

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -26,7 +26,7 @@ const AboutPage = () => {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#8646E6] to-[#5046E6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-12rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#8646E6] to-[#5046E6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -34,7 +34,7 @@ const AboutPage = () => {
           />
         </div>
         <div className="mx-5 pb-32 pt-10 sm:pb-20 md:pt-20">
-          <p className="prose mx-auto mb-32 text-justify md:mb-20">
+          <p className="prose mx-auto mb-32 sm:text-justify md:mb-20">
             <span className="text-2xl">Yurtle,</span> inspired by the enduring
             spirit of turtles and the community-centric ethos of Dr. Seuss's
             "Yertle the Turtle," champions the power of{' '}
@@ -60,7 +60,7 @@ const AboutPage = () => {
           <YurtleLogoOutline colored className="m-auto h-16 w-auto" />
         </div>
         <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          className="absolute inset-x-0 top-[calc(100%-50rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-45rem)]"
           aria-hidden="true"
         >
           <div
