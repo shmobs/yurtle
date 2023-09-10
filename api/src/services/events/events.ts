6183a18af37127877c142eeb51a8f3bb08d9bc6a
Ari Mendelow <ari@mendelow.me>
2023-09-10 16:34:12 -0700

    Fix bug where it was not returning the correct event status on rsvp/interest

--- api/src/services/events/events.ts
+++ api/src/services/events/events.ts
@@ -67,85 +67,89 @@ export const setEventInterestOrRSVP: MutationResolvers['setEventInterestOrRSVP']
     const dbModelBeingAccessed =
       action === 'INTEREST' ? 'eventInterest' : 'eventRSVP'
 
-    return db.$transaction(async (tx) => {
-      // need to cast this as any because otherwise TS gets tripped up on not knowing which type of accessor we're using.
-      // however, we know that their model in the database is identical, so this is fine
-      const dbActionAccessor = tx[dbModelBeingAccessed] as any
+    return db.$transaction(
+      async (tx) => {
+        // need to cast this as any because otherwise TS gets tripped up on not knowing which type of accessor we're using.
+        // however, we know that their model in the database is identical, so this is fine
+        const dbActionAccessor = tx[dbModelBeingAccessed] as any
 
-      const currentInterestsOrRSVPs = await dbActionAccessor.findMany({
-        where: {
-          eventId,
-        },
-      })
-
-      const isCurrentlyInterestedOrAttending =
-        !!(await dbActionAccessor.findFirst({
+        const currentInterestsOrRSVPs = await dbActionAccessor.findMany({
           where: {
             eventId,
-            userId: currentUserId,
           },
-        }))
+        })
 
-      if (isInterestedOrAttending) {
-        if (!isCurrentlyInterestedOrAttending) {
-          await dbActionAccessor.create({
-            data: {
-              user: {
-                connect: {
-                  id: currentUserId,
+        const isCurrentlyInterestedOrAttending =
+          !!(await dbActionAccessor.findFirst({
+            where: {
+              eventId,
+              userId: currentUserId,
+            },
+          }))
+
+        if (isInterestedOrAttending) {
+          if (!isCurrentlyInterestedOrAttending) {
+            await dbActionAccessor.create({
+              data: {
+                user: {
+                  connect: {
+                    id: currentUserId,
+                  },
+                },
+                event: {
+                  connect: {
+                    id: eventId,
+                  },
                 },
               },
-              event: {
-                connect: {
+            })
+
+            // if the user is interested, and they are the first, we need to update the event status
+            if (currentInterestsOrRSVPs.length === 0 && action === 'INTEREST') {
+              await tx.event.update({
+                where: {
                   id: eventId,
                 },
-              },
-            },
-          })
-
-          // if the user is interested, and they are the first, we need to update the event status
-          if (currentInterestsOrRSVPs.length === 0 && action === 'INTEREST') {
-            await tx.event.update({
+                data: {
+                  status: 'REQUESTED',
+                },
+              })
+            }
+          }
+        } else {
+          if (isCurrentlyInterestedOrAttending) {
+            await dbActionAccessor.delete({
               where: {
-                id: eventId,
-              },
-              data: {
-                status: 'REQUESTED',
+                eventId_userId: {
+                  eventId,
+                  userId: currentUserId,
+                },
               },
             })
+
+            // if the user is no longer interested, and they were the last, we need to update the event status
+            if (currentInterestsOrRSVPs.length === 1 && action === 'INTEREST') {
+              await tx.event.update({
+                where: {
+                  id: eventId,
+                },
+                data: {
+                  status: 'SUGGESTED',
+                },
+              })
+            }
           }
         }
-      } else {
-        if (isCurrentlyInterestedOrAttending) {
-          await dbActionAccessor.delete({
-            where: {
-              eventId_userId: {
-                eventId,
-                userId: currentUserId,
-              },
-            },
-          })
 
-          // if the user is no longer interested, and they were the last, we need to update the event status
-          if (currentInterestsOrRSVPs.length === 1 && action === 'INTEREST') {
-            await tx.event.update({
-              where: {
-                id: eventId,
-              },
-              data: {
-                status: 'SUGGESTED',
-              },
-            })
-          }
-        }
-      }
-
-      return db.event.findFirstOrThrow({
-        where: {
-          id: eventId,
-        },
-      })
-    })
+        const toReturn = await tx.event.findFirstOrThrow({
+          where: {
+            id: eventId,
+          },
+        })
+        return toReturn
+      },
+      { maxWait: 99999999999, timeout: 99999999999 }
+    )
   }
 
 export const Event: EventRelationResolvers = {
