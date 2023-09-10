f68be1935c9f72602437e9548143a83fe2a38bb4
Ari Mendelow <ari@mendelow.me>
2023-09-10 14:30:46 -0700

    Update business loading state

--- web/src/components/Business/Business.tsx
+++ web/src/components/Business/Business.tsx
@@ -5,23 +5,35 @@ import { cn } from 'src/lib/utils'
 
 import Locations from '../Locations/Locations'
 import SectionHeader from '../SectionHeader/SectionHeader'
+import { Skeleton } from '../ui/skeleton'
 
 interface IBusinessProps {
-  business: BusinessQuery['business']
+  business?: BusinessQuery['business']
 }
 const Business = ({ business }: IBusinessProps) => {
-  const { name, locations, description } = business
+  const { name, locations, description } = business || {}
   return (
     <>
-      <SimplePageHeader title={name} />
+      {name && <SimplePageHeader title={name} />}
       <div>
-        <p>{description}</p>
-        <SectionHeader
-          title="Venues"
-          subtitle={`${name}'s venues with events on Yurtle`}
-        />
+        <p>
+          {description || (
+            <>
+              <Skeleton className="mb-2 h-5" />
+              <Skeleton className="h-5" />
+            </>
+          )}
+        </p>
+        {name ? (
+          <SectionHeader
+            title="Venues"
+            subtitle={`${name}'s venues with events on Yurtle`}
+          />
+        ) : (
+          <SectionHeader className="mb-5" />
+        )}
         <Locations
-          locations={locations.map((loc) => ({
+          locations={locations?.map((loc) => ({
             id: loc.id,
             businessName: loc.address,
 
